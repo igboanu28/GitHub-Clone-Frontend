12a7088490ad6aa6b5a85878ae62a28ff005fc37
Igboanu Uchenna <igboanuuchenna@gmail.com>
2025-11-26 19:40:14 +0100

    added page for the issues part

---
+++ src/app/pages/issues/views/views.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-views',
+  imports: [],
+  templateUrl: './views.html',
+  styleUrl: './views.css',
+})
+export class Views {
+
+}
